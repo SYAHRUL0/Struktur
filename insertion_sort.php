9e0ce50b4ab875d881284db6528b1999ab761ce4
SYAHRUL0 <84725353+SYAHRUL0@users.noreply.github.com>
2021-06-03 21:00:30 +0700

    Update insertion_sort.php

--- insertion_sort.php
+++ insertion_sort.php
@@ -1,19 +1,43 @@
 <?php
 
-echo "//insertion sortn";
+
 $data=array(6,5,3,1,8,7,2,4);
+print_r($data);
+echo "<br>";
+
 function insertion_sort($data){
   $n=count($data);
-  for ($i = 1;$i<$n;$i++){ for ($k = $i; $k>0; $k--) {
+
+  echo "jumlah data : " .$n."<br>";
+
+
+  for ($i = 1;$i<$n;$i++)
+  
+  
+  
+  { 
+    echo "<br>";
+    echo "perulangan : ".$i."<br>";
+    
+    
+    for ($k = $i; $k>0; $k--)
+    
+    {
       if($data[$k]<$data[$k-1]){
+
+        echo "data ke : ".$k." : ".$data[$k]."ubah" .$data[$k-1]."=hasil ";
         $dummy=$data[$k];
         $data[$k]=$data[$k-1];
         $data[$k-1]=$dummy;
+        echo "<br>";
       }
     }
   }
   return $data;
 }
-print_r(insertion_sort($data));
+print_r($data);
+echo "<br>";
 
+$hasil =insertion_sort($data);
+print_r($hasil);
 ?>
